434ee45fbf4ea910dc94f51d47a08df2b82bb97b
Donatas Paplauskas <don@ecce.uk>
2019-08-30 10:27:31 +0100

    handling inputs

--- src/components/PollCreate.js
+++ src/components/PollCreate.js
@@ -5,22 +5,51 @@ class PollCreate extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      question: ''
+      question: '',
+      answers: []
     }
     this.handleInputChange = this.handleInputChange.bind(this)
   }
 
   handleInputChange(event) {
     const target = event.target
-    const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name;
 
-    this.setState({
-      [name]: value
-    })
+    if(target.name.startsWith('answer_')) {
+      const idx = parseInt(target.name.replace('answer_', ''))
+      if(target.value === '' && this.state.answers[idx] !== undefined) {
+        const arr = this.state.answers;
+        arr.splice(idx, 1)
+        this.setState({
+          answers: arr
+        })
+      } else {
+        const arr = this.state.answers
+        arr[idx] = target.value
+        this.setState({
+          answers: arr
+        })
+      }
+    } else {
+      this.setState({
+        [target.name]: target.value
+      })
+    }
   }
 
   render() {
+    const answersInputs = []
+    for(let i = 0; i < this.state.answers.length+1; i++) {
+      answersInputs.push(
+        <input
+          type="text"
+          key={i}
+          name={'answer_' + i}
+          placeholder="Answer"
+          value={this.state.answers[i] !== undefined ? this.state.answers[i] : ''}
+          onChange={this.handleInputChange}
+        />
+      )
+    }
     return (
       <div className="poll-create-form">
         <p>{this.state.question}</p>
@@ -32,8 +61,9 @@ class PollCreate extends React.Component {
           onChange={this.handleInputChange}
         />
         <div className="answers">
-          <input name="answer_1" placeholder="Answer" onChange={this.handleInputChange} />
+          {answersInputs}
         </div>
+        <button className="create-poll-button">Create</button>
       </div>
     )
   }
